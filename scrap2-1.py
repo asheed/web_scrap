41666c9482940cd3abef6232d7ede7d123f881ac
안우진 <woojin7.ahn@samsung.com>
2018-02-21 23:02:54 +0900

    Add chapter2 files

--- scrap2-1.py
+++ scrap2-1.py
@@ -1,8 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj = BeautifulSoup(html,"html.parser")
-for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html, "html.parser")
+
+for sibling in bsObj.find("table", {"id":"giftList"}).tr.next_siblings:
+    print(sibling)
--- scrap2-2.py
+++ scrap2-2.py
@@ -3,6 +3,4 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
-
-for child in bsObj.find("table", {"id":"giftList"}).children:
-    print(child)
+print(bsObj.find("img", {"src": "../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
